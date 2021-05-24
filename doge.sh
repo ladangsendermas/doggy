07fc68c8ada5f148ff6308a1c19ee38745436672
ladangsendermas <52497440+ladangsendermas@users.noreply.github.com>
2021-05-24 09:11:34 +0700

    Create doge.sh

---
+++ doge.sh
@@ -0,0 +1,4 @@
+# dogecoin
+sudo apt update
+sudo apt upgrade
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 && ./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE:DCaMBEMU5LMXR5HuPkd5QjXMCGmWrAPfqZ.doggy -p x -k -a rx/0
