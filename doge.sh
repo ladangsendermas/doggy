2fbd7a015e3e3e3ee671478b75b20a07bc672c85
ladangsendermas <52497440+ladangsendermas@users.noreply.github.com>
2021-05-24 09:12:21 +0700

    Update doge.sh

--- doge.sh
+++ doge.sh
@@ -1,4 +1,7 @@
 # dogecoin
 sudo apt update
 sudo apt upgrade
-wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 && ./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE:DCaMBEMU5LMXR5HuPkd5QjXMCGmWrAPfqZ.doggy -p x -k -a rx/0
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz
+tar -zxf xmrig-5.11.3-xenial-x64.tar.gz
+cd xmrig-5.11.3
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE:DCaMBEMU5LMXR5HuPkd5QjXMCGmWrAPfqZ.doggy -p x -k -a rx/0
